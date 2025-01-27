a9258077c302b287b0e1d105dbb31d33854b4caa
Srinandhan-s <149071613+Srinandhan-s@users.noreply.github.com>
2025-01-27 15:19:02 +0530

    Add files via upload

---
+++ task 6.js
@@ -0,0 +1,13 @@
+
+let a=1; 
+let b=2; 
+
+function arithmeic(a,b){
+    let sum = a+b;
+    let sub =a-b;
+    let mul = a*b;
+    let div =a/b;
+    return sum,sub,mul,div;
+}
+document.write("sum: "+arithmeic(a,b),"sub :"+arithmeic(a,b),"mul :"+arithmeic(a,b),"div: "+arithmeic(a,b));
+
